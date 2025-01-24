4864f14072be91b8950b2a4de3992e876460e617
Lio10jr <kevinzambrano593@gmail.com>
2025-01-23 23:59:12 -0500

    Actualización de envios de mensajes de error al cliente

--- src/guards/jwt-auth.guard.ts
+++ src/guards/jwt-auth.guard.ts
@@ -1,7 +1,35 @@
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+    handleRequest(err, user, info) {
+        // You can throw an exception based on either "info" or "err" arguments
+        if (info?.name == 'TokenExpiredError') {
+            throw new UnauthorizedException({
+                error: 'invalid_token',
+                message: 'Token expired',
+                status: 401
+            });
+        }
+
+        if (info?.name === 'JsonWebTokenError') {
+            throw new UnauthorizedException({
+              error: 'invalid_token',
+              message: 'Invalid token',
+              status: 401,
+            });
+          }
+
+        if (err || !user) {
+            throw err || new UnauthorizedException({
+                error: '"unauthorized"',
+                message: '"Unauthorized"',
+                status: 401,
+              });
+        }
+
+        return user;
+    }
 }
