113da53b1e651332b9d65989fca2b3c5e2ea56c4
Lio10jr <kevinzambrano593@gmail.com>
2025-01-24 00:00:53 -0500

    Configuración de dto para actualizar la información del usuario

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,6 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { RegisterAuthDto } from './register-auth.dto';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateAuthDto extends PartialType(RegisterAuthDto) {
-  id: number;
+  @IsOptional()
+  @IsString()
+  @IsEmail()
+  new_email: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  new_password: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  repit_new_password: string;
 }
