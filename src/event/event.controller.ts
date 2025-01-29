08dfa7a766f353f2b420f1b4294ea490ea718c34
Lio10jr <kevinzambrano593@gmail.com>
2025-01-28 19:59:55 -0500

    Organización y verificación de funciones de eventos

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -4,7 +4,7 @@ import { EVENT_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { JwtAuthGuard } from 'src/guards';
-import { CurrentUser, CustomException } from 'src/common';
+import { CurrentUser, CustomException, PaginationDto } from 'src/common';
 import { User } from 'src/auth/entities';
 
 @Controller('event')
@@ -21,28 +21,6 @@ export class EventController {
     @CurrentUser() user: User
   ) {
     return this.clientEvent.send('createEvent', { ...createEventDto, userId: user.id})
-    .pipe(catchError(error => { throw new RpcException(error) }));
-  }
-
-  @Get()
-  findAll() {
-    return this.clientEvent.send('findAllEvent', {})
-    .pipe(catchError(error => { throw new RpcException(error) }));
-  }
-
-  @Get('for-user')
-  @UseGuards(JwtAuthGuard)
-  findForUser(@CurrentUser() user: User) {
-    return this.clientEvent.send('findForUserEvent', user.id)
-    .pipe(catchError(error => { throw new RpcException(error) }));
-  }
-
-  @Get(':id')
-  @UseGuards(JwtAuthGuard)
-  findOne(
-    @Param('id', ParseIntPipe) id: number, 
-  ) {
-    return this.clientEvent.send('findOneEvent', id)
     .pipe(catchError(error => { throw new RpcException(error) }));
   }
 
@@ -66,4 +44,46 @@ export class EventController {
     return this.clientEvent.send('removeEvent', { userId: user.id, id })
     .pipe(catchError(error => { throw new RpcException(error) }));
   }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  findOne(
+    @Param('id', ParseIntPipe) id: number, 
+  ) {
+    return this.clientEvent.send('findOneEvent', id)
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  }
+
+  @Get('for-user')
+  @UseGuards(JwtAuthGuard)
+  findByUser(
+    @Body() pagination: PaginationDto,
+    @CurrentUser() user: User) {
+    return this.clientEvent.send('findByUserEvent', { id: user.id, pagination})
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  }
+
+  @Get()
+  findAll() {
+    return this.clientEvent.send('findAllEvent', {})
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  }
+
+  @Get('now')
+  findAllNow(@Body() pagination: PaginationDto) {
+    return this.clientEvent.send('findAllNowEvent', pagination)
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  }
+
+  @Get('today')
+  findAllToday(@Body() pagination: PaginationDto) {
+    return this.clientEvent.send('findAllTodayEvent', pagination)
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  } 
+  
+  @Get('programmed')
+  findAllProgrammed(@Body() pagination: PaginationDto) {
+    return this.clientEvent.send('findAllProgrammedEvent', pagination)
+    .pipe(catchError(error => { throw new RpcException(error) }));
+  }  
 }
