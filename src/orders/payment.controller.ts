c62682e1bd4d18e311ff17db90872485b205d98a
Lio10jr <kevinzambrano593@gmail.com>
2025-03-03 23:56:01 -0500

    feat: Simplificar PaymentController eliminando métodos obsoletos y añadiendo webhook para Stripe

--- src/orders/payment.controller.ts
+++ src/orders/payment.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Inject, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Inject, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { ORDER_SERVICE } from 'src/config';
-import { CreatePaymentDto, ConfirmPaymentDto } from './dto/payment';
 import { EventDto } from './dto';
 import { JwtAuthGuard } from 'src/guards';
 import { CurrentUser } from 'src/common';
@@ -14,36 +13,6 @@ export class PaymentController {
         @Inject(ORDER_SERVICE) private readonly clientOrder: ClientProxy,
     ) { }
 
-    @Get('customer/:customerId')
-    getPayments(@Param('customerId') customerId: string) {
-        return this.clientOrder.send('getPayments', customerId)
-            .pipe(catchError(error => { throw new RpcException(error) }));
-    }
-
-    @Get('customer/:customerId/payment/:paymentId')
-    getPayment(
-        @Param('customerId') customerId: string,
-        @Param('paymentId') paymentId: string) {
-        return this.clientOrder.send('getPayment', {customerId, paymentId})
-            .pipe(catchError(error => { throw new RpcException(error) }));
-    }
-
-    @Post('customer/:customerId')
-    create(
-        @Param('customerId') customerId: string,
-        @Body() createPaymentIntentDto: CreatePaymentDto) {
-        return this.clientOrder.send('createPayment', {customerId, createPaymentIntentDto})
-            .pipe(catchError(error => { throw new RpcException(error) }));
-    }
-
-    @Post('user/:userId')
-    confirmPayment(
-        @Param('userId', ParseIntPipe) userId: number,
-        @Body() confirmPaymentDto: ConfirmPaymentDto) {
-        return this.clientOrder.send('confirmPayment', {userId, confirmPaymentDto})
-            .pipe(catchError(error => { throw new RpcException(error) }));
-    }
-
     @Post('create-checkout-session')
     @UseGuards(JwtAuthGuard)
     createCheckoutSession(
@@ -54,6 +23,13 @@ export class PaymentController {
     ) {
         return this.clientOrder.send('createCheckoutSessionPayment', 
             { event, cuid, quantity, customer: { userId: user.id, email: user.email, name: user.name + ' ' + user.lastname }})
+            .pipe(catchError(error => { throw new RpcException(error) }));
+    }
+
+    // modern-awe-amazed-crisp
+    @Post('webhookStripe')
+    webhookStripe(@Body() event: any ) {
+        return this.clientOrder.send('webhookPayment', event)
             .pipe(catchError(error => { throw new RpcException(error) }));
     }
 }
